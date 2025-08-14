69b3f4719cfcd53b61a97881d654274ba0d7de69
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-13 22:53:37 -0300

    Classe CategoriaDAO

---
+++ src/main/java/br/fiap/conexao/Conexao.java
@@ -0,0 +1,24 @@
+package br.fiap.conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLDataException;
+import java.sql.SQLException;
+
+public class Conexao {
+    private static String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
+    private static String USER = "";
+    private static String PASSWORD = "";
+
+    private Conexao() {}
+
+    public static Connection conectar() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        catch(SQLException e) {
+            System.out.println("Erro ao conectar no banco\n" + e);
+        }
+        return null;
+    }
+}
